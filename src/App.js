cebe18029e1ff2e560dc4843206af3e455a8781e
HighRanger11 <jaemmu11@gmail.com>
2021-12-31 19:17:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+
+    </div>
+  );
+}
+
+export default App;
